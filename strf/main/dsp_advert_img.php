ffda7b17b5772ecf573fbb6d782ca45c9550d3e8
serjoga <serjoga@serjoga-MS-7309>
2013-03-03 22:35:45 +0200

    bags & del

--- strf/main/dsp_advert_img.php
+++ strf/main/dsp_advert_img.php
@@ -7,7 +7,7 @@ function get_Advert($width, $height, $advert_array, $pos){
     
     $com = rand(0, (count($advert_array)-1));
     
-    $str = '<form action="http://'.$advert_array[$com][$pos]['where_from'].'" method="post"  target="_blank">
+    $str = '<form action="'.$advert_array[$com][$pos]['where_from'].'" method="post"  target="_blank">
             <input type="image" src="http://shop.po-mera.ru'.$advert_array[$com][$pos]['name'].'" width="'.$width.'" height="'.$height.'" alt="'.$advert_array[$com][$pos]['name'].'"/>        
         </form>';
     
--- strf/main/dsp_cart_plus.php
+++ strf/main/dsp_cart_plus.php
@@ -4,7 +4,7 @@
 		<div class =  "img_tovar_chk">
                     <div id = "name_tovara_chk"><p align="center"><strong><?php echo $status;?></strong></p></div>
 			<div id = "img_to_chk">
-                            <img src="main/act_prewiew.php?src=http://<?php echo $_SESSION[domen];?>/images/goods/<?php echo $img; ?>.jpg&width=120&height=160" alt="Images"/>
+                            <img src="main/act_prewiew.php?src=http://<?php echo $_SERVER['SERVER_NAME'];?>/images/goods/<?php echo $img; ?>.jpg&width=120&height=160" alt="Images"/>
                         </div>
 			<div id = "name_tovara_chk"><?php echo $name;?></div>
 			<div id = "krat_opis_chk"><?php echo $comment;?></div>
@@ -26,12 +26,12 @@
                                    
                                     <form action="index.php?act=change_volume" method="post">
                                         <p align="center">
-                                            <input type="image" src="<?php echo "http://$_SESSION[domen]";?>/images/storefront/btn_up.gif" alt="Изменить." name="submit" value="val" style="vertical-align: middle" <?php //echo $dis;?>/></p>
+                                            <input type="image" src="<?php echo "http://{$_SERVER['SERVER_NAME']}";?>/images/storefront/btn_up.gif" alt="Изменить." name="submit" value="val" style="vertical-align: middle" <?php //echo $dis;?>/></p>
                                             <input type="hidden" name="name" value="<?php echo $name;?>"/>                                        
                                             <?php if(isset ($attributes[cod])){ ?> <input type="hidden" name="cod" value="<?php echo $attributes[cod]; ?>"/><?php } ?>                                        
                                             <input type="hidden" name="stid" value="<?php echo $attributes[stid];?>"/>
                                             <input type="hidden" name="value" value="<?php echo $volume;?>"/>
-                                            <input type="hidden" name="art_n" value="<?php echo $list_volume[$posart + 1][str_code1];?>"/>
+                                            <input type="hidden" name="art_n" value="<?php echo $list_volume[$posart + 1]['str_code1'];?>"/>
                                             <input type="hidden" name="art_o" value="<?php echo $artikul;?>"/>
                                             <input type="hidden" name="price_id" value="<?php echo $price_id;?>"/>
                                             <input type="hidden" name="type" value="1"/>
@@ -40,24 +40,24 @@
                                     <?php
                                     
                                     $dis = ''; }else{?>
-                                    <p align="center"><img  src="<?php echo "http://$_SESSION[domen]";?>/images/storefront/btn_up.gif" alt="Изменить." /></p>
+                                    <p align="center"><img  src="<?php echo "http://{$_SERVER['SERVER_NAME']}";?>/images/storefront/btn_up.gif" alt="Изменить." /></p>
                                     <?php }?>
                                 </div>
 				<div id = "vid_chk"><p class="cng_volume"><?php echo $volume;?> кг. </p></div>
 				<div id = "up_vid_chk">
-                                     <?php if($count_volume>1  && $list_volume[$posart - 1][str_code1]){ 
+                                     <?php if($count_volume>1  && $list_volume[$posart - 1]['str_code1']){ 
      
 //                                         if(!$list_volume[$posart - 1][str_code1]) $dis = "disabled";
                                          
                                          ?>
                                     <form action="index.php?act=change_volume" method="post">
                                         <p align="center">
-                                             <input type="image" src="<?php echo "http://$_SESSION[domen]";?>/images/storefront/btn_down.gif" alt="Удалить" name="submit" value="val" style="vertical-align: middle" <?php //echo $dis;?>/>
+                                             <input type="image" src="<?php echo "http://{$_SERVER['SERVER_NAME']}";?>/images/storefront/btn_down.gif" alt="Удалить" name="submit" value="val" style="vertical-align: middle" <?php //echo $dis;?>/>
                                             <input type="hidden" name="name" value="<?php echo $name;?>"/>
-                                            <?php if(isset ($attributes[cod])){ ?> <input type="hidden" name="cod" value="<?php echo $attributes[cod]; ?>"/><?php } ?>
-                                            <input type="hidden" name="stid" value="<?php echo $attributes[stid];?>"/>
+                                            <?php if(isset ($attributes[cod])){ ?> <input type="hidden" name="cod" value="<?php echo $attributes['cod']; ?>"/><?php } ?>
+                                            <input type="hidden" name="stid" value="<?php echo $attributes['stid'];?>"/>
                                             <input type="hidden" name="value" value="<?php echo $volume;?>"/>
-                                            <input type="hidden" name="art_n" value="<?php echo $list_volume[$posart - 1][str_code1];?>"/>
+                                            <input type="hidden" name="art_n" value="<?php echo $list_volume[$posart - 1]['str_code1'];?>"/>
                                             <input type="hidden" name="art_o" value="<?php echo $artikul;?>"/>
                                             <input type="hidden" name="price_id" value="<?php echo $price_id;?>"/>
                                             <input type="hidden" name="type" value="0"/>
@@ -65,7 +65,7 @@
                                         </p>
                                     </form>
                                     <?php $dis = "";}else{?>
-                                    <p align="center"><img src="<?php echo "http://$_SESSION[domen]";?>/images/storefront/btn_down.gif" alt="Добвить" /></p>
+                                    <p align="center"><img src="<?php echo "http://{$_SERVER['SERVER_NAME']}";?>/images/storefront/btn_down.gif" alt="Добвить" /></p>
                                     <?php }?>
                                 </div>
 			</div>
@@ -78,13 +78,13 @@
                                         <p align="center">
                                             <input type="hidden"  name="artikul" value="<?php echo $artikul;?>"/>
                                             <input type="hidden"  name="pricelist_id" value="<?php echo $price_id;?>"/>
-                                            <input type="hidden" name="cod" value="<?php echo $attributes[cod]; ?>"/>
-                                            <input type="hidden" name="stid" value="<?php echo $attributes[stid];?>"/>
+                                            <input type="hidden" name="cod" value="<?php echo $attributes['cod']; ?>"/>
+                                            <input type="hidden" name="stid" value="<?php echo $attributes['stid'];?>"/>
                                             <input type="hidden"  name="discount" value="0"/>
                                             <input type="hidden"  name="up" value="1"/>
                                             <input type="hidden"  name="amount" value="1"/>
                                             <input type="hidden"  name="type" value="2"/>
-                                            <input type="image" src="<?php echo "http://$_SESSION[domen]";?>/images/storefront/btn_up.gif" alt="Добвить" name="submit0" value="val" style="vertical-align: middle"/>
+                                            <input type="image" src="<?php echo "http://{$_SERVER['SERVER_NAME']}";?>/images/storefront/btn_up.gif" alt="Добвить" name="submit0" value="val" style="vertical-align: middle"/>
                                         </p>
                                     </form>
                                 </div>
@@ -94,13 +94,13 @@
                                         <p align="center">
                                             <input type="hidden"  name="artikul" value="<?php echo $artikul;?>"/>
                                             <input type="hidden"  name="pricelist_id" value="<?php echo $price_id;?>"/>
-                                            <input type="hidden" name="stid" value="<?php echo $attributes[stid];?>"/>
+                                            <input type="hidden" name="stid" value="<?php echo $attributes['stid'];?>"/>
                                             <input type="hidden"  name="discount" value="0"/>
                                             <input type="hidden"  name="amount" value="1"/>
                                             <input type="hidden"  name="down" value="1"/>
                                             <input type="hidden"  name="type" value="2"/>
-                                            <input type="hidden"  name="cod" value="<?php echo $attributes[cod];?>"/>
-                                            <input type="image" src="<?php echo "http://$_SESSION[domen]";?>/images/storefront/btn_down.gif" alt="Удалить" name="submit1" value="val" style="vertical-align: middle"/>
+                                            <input type="hidden"  name="cod" value="<?php echo $attributes['cod'];?>"/>
+                                            <input type="image" src="<?php echo "http://{$_SERVER['SERVER_NAME']}";?>/images/storefront/btn_down.gif" alt="Удалить" name="submit1" value="val" style="vertical-align: middle"/>
                                         </p>
                                     </form>
                                 </div>
@@ -114,9 +114,9 @@
 <?php 
                         $str = "act=del_item&artikul=$artikul&stid=$attributes[stid]&type=$type";
                         
-                        if(!isset ($_SESSION[auth]) or $_SESSION[auth] == 0){
+                        if(!isset ($_SESSION['auth']) or $_SESSION['auth'] == 0){
                            
-                            $str = $str."&cod=".$attributes[cod];
+                            $str = $str."&cod=".$attributes['cod'];
                         }
                         
                         ?>
